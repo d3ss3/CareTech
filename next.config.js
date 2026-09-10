b6f41b2e43ed1242be9fd05eada2643260ebe676
d3ss3 <90552858+d3ss3@users.noreply.github.com>
2026-09-10 13:55:40 +0300

    Update next.config.js to include image settings
    Added unoptimized images configuration to Next.js.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+// ترقيع الشاشات القياسية لبيئات التشغيل الافتراضية
+['stdout', 'stderr'].forEach((stream) => {
+  if (process[stream]) {
+    if (!process[stream]._handle) {
+      process[stream]._handle = {};
+    }
+    process[stream]._handle.setBlocking = () => {};
+  }
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  };
-  
-  module.exports = nextConfig;
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
